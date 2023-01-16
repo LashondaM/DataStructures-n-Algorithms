c9805e655f32e4862b0bd4234637b531843db38f
Lashonda Miller <lashondamiller007@gmail.com>
2023-01-16 14:29:29 -0500

    Big-O Notation notes disclaimer

--- DataStructures-n-Algorithms/Program.cs
+++ DataStructures-n-Algorithms/Program.cs
@@ -6,3 +6,10 @@ using System.Collections;
 Stack ts = new Stack();
 
 Pokemon squirtle = new Pokemon();
+
+
+// ----------
+
+/* Big-O Notations - How efficient is your code, how fast and how well does it run? 
+   Notes were written down in my notebook for that as well as on discord for examples.
+*/
