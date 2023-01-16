c81f51cbafbf8e9041e2bc9b1ca4524e1a926946
Lashonda Miller <lashondamiller007@gmail.com>
2023-01-16 12:54:05 -0500

    Abstract Data Types

---
+++ DataStructures-n-Algorithms/ADT/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_n_Algorithms.ADT
+{
+    public class Pokemon
+    {
+        // Representation
+        public string Name { get; set; }
+        public int Exp { get; set; }
+
+        // Operation
+        public bool IsPokemon()
+        {
+            return true;
+        }
+    }
+}
--- DataStructures-n-Algorithms/Program.cs
+++ DataStructures-n-Algorithms/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using DataStructures_n_Algorithms.ADT;
+using System.Collections;
+
+Stack ts = new Stack();
+
+Pokemon squirtle = new Pokemon();
