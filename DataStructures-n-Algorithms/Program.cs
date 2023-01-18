f944c68a5ff0b79ef86e10081e6acb83c27d37f7
Lashonda Miller <lashondamiller007@gmail.com>
2023-01-18 09:10:31 -0500

    Array + Array Insertions

--- DataStructures-n-Algorithms/Program.cs
+++ DataStructures-n-Algorithms/Program.cs
@@ -1,15 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Arrays
+//Array Insertions and Deletions
 
-using DataStructures_n_Algorithms.ADT;
-using System.Collections;
+// Inserting at the end of an Array O(1)
 
-Stack ts = new Stack();
+int[] intArray = new int[10];
 
-Pokemon squirtle = new Pokemon();
+// Make a variable to keep the length because .Length is based off capacity and does not track the actual index
+int length = 0;
+
+//This adds data for us
+for (int i = 0; i < 8; i++)
+{
+    //1 is optional
+    intArray[length] = i + 1;
+    length++; // iterate so we put values inside the array and keep track of the length
+}
+
+/* insertion for the end of the array */
+//intArray[length] = 8;
+//length++;
 
 
-// ----------
+/* ============ Inserting at the start of an Array ============ */
+/* for (int i = 3; i >= 0; i--)
+{
+    // this is moving over all the values in the array
+    intArray[i + 1] = intArray[i];
+}
 
-/* Big-O Notations - How efficient is your code, how fast and how well does it run? 
-   Notes were written down in my notebook for that as well as on discord for examples.
+intArray[0] = 20;
+
+// this is so we can see the 20 get assigned before the Watch window closes
+int value = 0;
+
 */
+
+
+/* ============ Inserting anywhere in the array ============ */
+for (int i = 4; i >= 2; i--)
+{
+    //Shift each element one position to the right
+    intArray[i + 1] = intArray[i];
+}
+
+intArray[2] = 8;
+
+int value = 0;
